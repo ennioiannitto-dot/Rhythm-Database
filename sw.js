9a1ba628159ac6e944210712a2b9983ceb8e7ca0
ennioiannitto-dot <ennio.iannitto@gmail.com>
2026-07-31 12:14:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'rhythm-database-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e)=>{
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached=>{
+      if(cached) return cached;
+      return fetch(e.request).then(resp=>{
+        const clone = resp.clone();
+        caches.open(CACHE_NAME).then(cache=>cache.put(e.request, clone));
+        return resp;
+      }).catch(()=>cached);
+    })
+  );
+});
